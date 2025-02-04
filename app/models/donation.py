0a73eb23d732015c428a4626545256db8ba4b3b4
Евгений Братанов <sonik.zhenyaa@gmail.com>
2025-02-04 19:12:07 +0300

    Модель Donat

--- app/models/donation.py
+++ app/models/donation.py
@@ -6,5 +6,20 @@ __all__ = ["Donation"]
 
 
 class Donation(BaseModel):
+    """
+    Модель для пожертвований:
+    - user_id: ID пользователя, сделавшего пожертвование.
+    - comment: Комментарий к пожертвованию.
+    """
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     comment = Column(Text)
+
+    def __repr__(self):
+        return f"""<Donation(id={self.id},
+        user_id={self.user_id},
+        full_amount={self.full_amount})>"""
+
+    def __str__(self):
+        return f"""Donation (ID: {self.id},
+        User ID: {self.user_id},
+        Amount: {self.full_amount})"""
