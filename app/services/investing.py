b9437bd602aa6422aa5449d8f9633fa2a8e7c9c3
Евгений Братанов <sonik.zhenyaa@gmail.com>
2025-02-04 19:34:49 +0300

    Объединение

--- app/services/investing.py
+++ app/services/investing.py
@@ -1,109 +1,69 @@
 from datetime import datetime
-
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.crud.charity_project import charity_project_crud
-from app.schemas.charity_project import CharityProjectDB
-from app.schemas.donation import DonationDB
+
+
+async def invest_funds(
+    source,
+    target_getter,
+    session: AsyncSession,
+):
+    """
+    Общая логика распределения средств между источником и целями.
+    """
+    remaining_funds = source.full_amount - source.invested_amount
+    invested_amount = 0
+
+    while remaining_funds > 0:
+        target = await target_getter(session)
+        if target is None:
+            break
+
+        needed_funds = target.full_amount - target.invested_amount
+
+        if needed_funds <= remaining_funds:
+            target.invested_amount += needed_funds
+            invested_amount += needed_funds
+            remaining_funds -= needed_funds
+
+            target.fully_invested = True
+            target.close_date = datetime.now()
+        else:
+            target.invested_amount += remaining_funds
+            invested_amount += remaining_funds
+            remaining_funds = 0
+
+    source.invested_amount += invested_amount
+    if remaining_funds == 0:
+        source.fully_invested = True
+        source.close_date = datetime.now()
+
+    return source
 
 
 async def distribute_new_donation(
-        donation: DonationDB,
-        session: AsyncSession,
+    donation,
+    session: AsyncSession,
 ):
     """
     Распределяет новую пожертвованную сумму (donation) на открытые проекты.
-
-    Логика:
-    - Берется самый старый открытый проект.
-    - Пожертвование распределяется на проект до тех пор,
-    пока не будет полностью использовано
-      или пока не закончатся открытые проекты.
-    - Если проект полностью профинансирован, он закрывается
-    (устанавливается `fully_invested` и `close_date`).
     """
-    project = await charity_project_crud.get_oldest_open_project(session)
-    if project is None:
-        return donation
-
-    remaining_donation_amount = donation.full_amount
-    invested_amount = 0
-
-    while remaining_donation_amount > 0:
-        project = await charity_project_crud.get_oldest_open_project(session)
-        if project is None:
-            break
-
-        need_donation = project.full_amount - project.invested_amount
-        if need_donation <= remaining_donation_amount:
-            project.invested_amount += need_donation
-            invested_amount += need_donation
-            remaining_donation_amount -= need_donation
-        else:
-            project.invested_amount += remaining_donation_amount
-            invested_amount += remaining_donation_amount
-            remaining_donation_amount = 0
-
-        if project.invested_amount >= project.full_amount:
-            project.fully_invested = True
-            project.close_date = datetime.now()
-
-    donation.invested_amount = invested_amount
-    if remaining_donation_amount == 0:
-        donation.close_date = datetime.now()
-        donation.fully_invested = True
-    await session.commit()
-    return donation
+    return await invest_funds(
+        source=donation,
+        target_getter=charity_project_crud.get_oldest_open_project,
+        session=session,
+    )
 
 
 async def allocate_to_new_project(
-        project: CharityProjectDB,
-        session: AsyncSession,
+    project,
+    session: AsyncSession,
 ):
     """
     Распределяет доступные пожертвования на новый проект.
-
-    Логика:
-    - Берется самое старое открытое пожертвование.
-    - Пожертвование распределяется на проект до тех пор,
-    пока проект не будет полностью профинансирован
-      или пока не закончатся доступные пожертвования.
-    - Если проект полностью профинансирован, он закрывается
-    (устанавливается `fully_invested` и `close_date`).
     """
-    need_donation = project.full_amount
-
-    if need_donation <= 0:
-        await session.commit()
-        return project
-
-    while True:
-        donation = await charity_project_crud.get_oldest_open_donation(session)
-        if donation is None:
-            break
-
-        free_donation = donation.full_amount - donation.invested_amount
-
-        if free_donation < need_donation:
-            project.invested_amount += free_donation
-            donation.invested_amount = donation.full_amount
-            donation.fully_invested = True
-            donation.close_date = datetime.now()
-            need_donation -= free_donation
-
-        elif free_donation == need_donation:
-            project.invested_amount = project.full_amount
-            donation.invested_amount = donation.full_amount
-            donation.fully_invested = True
-            donation.close_date = datetime.now()
-            project.fully_invested = True
-            project.close_date = datetime.now()
-            break
-
-        elif free_donation > need_donation:
-            project.invested_amount = project.full_amount
-            donation.invested_amount += need_donation
-            project.fully_invested = True
-            project.close_date = datetime.now()
-            break
-    return project
+    return await invest_funds(
+        source=project,
+        target_getter=charity_project_crud.get_oldest_open_donation,
+        session=session,
+    )
